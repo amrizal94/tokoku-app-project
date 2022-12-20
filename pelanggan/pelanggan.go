c222ae0dd757f4093b9aad8f306ae564f9515585
rafli99 <rafli.muh015@gmail.com>
2022-12-20 14:39:34 +0700

    baru3

--- pelanggan/pelanggan.go
+++ pelanggan/pelanggan.go
@@ -1,9 +1,35 @@
 package pelanggan
 
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
+
 type Pelanggan struct {
 	hp         string
 	id_pegawai int
 	nama       string
+}
+
+type AuthPelanggan struct {
+	db *sql.DB
+}
+
+// Register implements PelangganInterface
+func (*AuthPelanggan) Register(newPelanggan Pelanggan) (bool, int, error) {
+	panic("unimplemented")
+}
+
+type PelangganInterface interface {
+	Register(newPelanggan Pelanggan) (bool, int, error)
+	Duplicate(id_pegawai string) (int, int8)
+}
+
+func NewPelangganMenu(conn *sql.DB) PelangganInterface {
+	return &AuthPelanggan{
+		db: conn,
+	}
 }
 
 func (p *Pelanggan) SetHP(newHP string) {
@@ -24,4 +50,32 @@ func (p *Pelanggan) GetIDPegawai() int {
 }
 func (p *Pelanggan) GetName() string {
 	return p.nama
+}
+
+func (ap *AuthPelanggan) Register1(newPelanggan Pelanggan) (bool, int, error) {
+	registerQry, err := ap.db.Prepare("INSERT INTO pelanggan (hp, id_pegawai, nama) values (?,?,?)")
+	if err != nil {
+		log.Println("prepare insert pegawai registerQry", err.Error())
+		return false, 0, errors.New("prepare statement insert pelanggan error registerQry")
+	}
+
+	// menjalankan query dengan parameter tertentu
+	res, err := registerQry.Exec(newPelanggan.GetHP(), newPelanggan.GetIDPegawai(), newPelanggan.GetName())
+	if err != nil {
+		log.Println("insert pelanggan registerQry ", err.Error())
+		return false, 0, errors.New("insert pelanggan error registerQry")
+	}
+	// Cek berapa baris yang terpengaruh query diatas
+	affRows, err := res.RowsAffected()
+	if err != nil {
+		log.Println("after insert username registerQry ", err.Error())
+		return false, 0, errors.New("error setelah insert registerQry")
+	}
+
+	if affRows <= 0 {
+		log.Println("no record affected registerQry")
+		return true, 0, errors.New("no record registerQry")
+	}
+
+	return true, 0, nil
 }
